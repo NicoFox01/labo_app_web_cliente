c3a1e426ec56ce76f58550c47f82e6e3996e5c61
Federico <fedev995@gmail.com>
2026-04-13 19:26:50 -0300

    feature_ modal de productos + limpieza de imágenes

--- scripts/ecommerce.js
+++ scripts/ecommerce.js
@@ -19,7 +19,7 @@ getAllProductos()
     productos.forEach((producto) => {
       listadoProductosCards += `
         <div class="col">
-          <div class="card h-100">
+          <div class="card h-100 product-card">
             <img src="${producto.image}" class="card-img-top" alt="${producto.title}" />
             <div class="card-body d-flex flex-column">
               
@@ -36,7 +36,7 @@ getAllProductos()
                   ⭐ ${producto.rating.rate} (${producto.rating.count})
                 </div>
 
-                <button class="btn btn-primary w-100">
+                <button class="btn btn-primary w-100" onclick="event.stopPropagation()">
                   <i class="bi bi-cart-plus"></i> Agregar
                 </button>
               </div>
@@ -48,8 +48,33 @@ getAllProductos()
     });
 
     listadoProductos.innerHTML = listadoProductosCards;
+    const cards = document.querySelectorAll(".product-card");
+
+cards.forEach((card, index) => {
+   card.addEventListener("click", () => {
+    console.log("CLICK EN CARD", productos[index]);
+    openProductModal(productos[index]);
+});
+});
   })
   .catch((error) => {
     listadoProductos.innerHTML = "<p>Error cargando productos</p>";
     console.error(error);
   });
+
+  function openProductModal(producto) {
+    document.getElementById("modal-title").innerText = producto.title;
+    document.getElementById("modal-price").innerText = "$" + producto.price;
+    document.getElementById("modal-description").innerText = producto.description;
+    document.getElementById("modal-img").src = producto.image;
+
+    document.getElementById("productModal").style.display = "flex";
+}
+
+function closeProductModal() {
+    document.getElementById("productModal").style.display = "none";
+}
+
+function addToCartAndClose() {
+    closeProductModal();
+}
